d1a8ced59d386c2707b382e4ce558292ce330e93
Trevor Hudson <trevhud@gmail.com>
2013-07-05 18:30:49 -0700

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Omrails::Application.routes.draw do
+
+  root :to => 'pages#home'
+  get 'about' => 'pages#about'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
+  
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
