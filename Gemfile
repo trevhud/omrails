4e96753adf156c0dc29c63d6fc9dabd052e95ef3
Trevor Hudson <trevhud@gmail.com>
2013-08-24 21:15:28 -0400

    trying papercip shit

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '3.2.12'
 gem 'jquery-rails'
 gem 'devise'
 gem 'simple_form'
-gem 'paperclip', '~> 3.5.0'
+gem 'paperclip', '~> 3.0'
 gem 'aws-sdk'
 
 # gem 'protected_attributes'
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,7 +4,9 @@ class Pin < ActiveRecord::Base
 
   validates :description, presence: true
   validates :user_id, presence: true
-  has_attached_file :image, styles: { medium: "320x240>"}
+  has_attached_file :image, styles: { medium: "320x240>"},
+  	:path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+    :url => "/system/:attachment/:id/:style/:filename"
   validates_attachment :image, presence: true,
   														 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
   														 size: { less_than: 5.megabytes }
